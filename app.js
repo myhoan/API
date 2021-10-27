fc011524286d3987f5ef4a52a56c4c724b161bff
Hoan Dinh <myhoan2805@gmail.com>
2021-10-27 14:40:57 +0700

    done

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 //Routes
 const postsRoute = require('./routes/posts');
--- routes/posts.js
+++ routes/posts.js
@@ -25,4 +25,37 @@ router.post('/', async (req, res) => {
     }
 });
 
+//id
+router.get('/:postId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    } catch (err) {
+        res.json({ message: err});
+    }
+});
+
+//delete 
+router.delete('/:postId', async (req, res) => {
+    try {
+        const removedPost = await Post.remove({_id: req.params.postId});
+        res.json(removedPost);
+    } catch (err) {
+        res.json({ message: err});
+    }
+});
+
+//update 
+router.patch('/:postId', async (req, res) => {
+    try {
+        const updatedPost = await Post.updateOne(
+            {_id: req.params.postId},
+            { $set: {title: req.body.title}}
+        );
+        res.json(updatedPost);
+    } catch (err) {
+        res.json({ message: err});
+    }
+});
+
 module.exports = router;
